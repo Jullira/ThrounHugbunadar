ae63ee61f6ad005ad027e23890a504c924c4474c
Biggatron <32528225+Biggatron@users.noreply.github.com>
2018-04-16 16:14:48 +0000

    Bæta við restina af scenunum og controllerum fyrir þau. Það á eftir að útlits fegra þau.

--- bokunarforrit/src/bokunarforrit/BokunUpplController.java
+++ bokunarforrit/src/bokunarforrit/BokunUpplController.java
@@ -7,7 +7,13 @@ package bokunarforrit;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -16,12 +22,43 @@ import javafx.fxml.Initializable;
  */
 public class BokunUpplController implements Initializable {
 
+    @FXML
+    private ImageView banner;
+    @FXML
+    private TextField nameTextField;
+    @FXML
+    private TextField adressTextField;
+    @FXML
+    private TextField postCodeTextField;
+    @FXML
+    private TextField cityTextField;
+    @FXML
+    private TextField countryTextField;
+    @FXML
+    private TextField phoneNumberTextField;
+    @FXML
+    private TextField emailTextField;
+    @FXML
+    private Text hotelNafn;
+    @FXML
+    private Label fromDateLabel;
+    @FXML
+    private Label toDateLabel;
+    @FXML
+    private Label numGuestsLabel;
+    @FXML
+    private Label totalPriceLabel;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
+
+    @FXML
+    private void confirmHandler(ActionEvent event) {
+    }
     
 }
---
+++ bokunarforrit/src/bokunarforrit/StadfestingController.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bokunarforrit;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author birgi
+ */
+public class StadfestingController implements Initializable {
+
+    @FXML
+    private ImageView banner;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void backOnFrontPageHandler(ActionEvent event) throws IOException {
+        newHomePage(event);
+    }
+    
+    public void newHomePage(ActionEvent event) throws IOException {
+        Parent homePage = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Scene homePageScene = new Scene(homePage);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(homePageScene);
+        stage.show();
+    }
+    
+}
